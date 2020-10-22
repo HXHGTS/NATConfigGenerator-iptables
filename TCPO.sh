379178254e782bfeab0039ac9ec28bcb27ec9b53
HXHGTS <38959826+HXHGTS@users.noreply.github.com>
2020-10-22 16:10:59 +0800

    Create TCPO.sh

---
+++ TCPO.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+echo "正在优化大文件读写性能. . ."
+
+curl https://raw.githubusercontent.com/HXHGTS/NATConfigGenerator-iptables/master/limits.conf > /etc/security/limits.conf
+
+echo "正在优化文件io性能. . ."
+
+curl https://raw.githubusercontent.com/HXHGTS/NATConfigGenerator-iptables/master/sysctl.conf > /etc/sysctl.conf
+
+sysctl -p
+
+echo "优化完成!"
+
